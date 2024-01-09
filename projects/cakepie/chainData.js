08d9cc1ee25d509a49a0d2879d588074a3d1d05f
siddhpurakaran <siddhpurakaran.sk@gmail.com>
2024-01-09 23:58:57 +0530

    Updated Cakepie adapter

--- projects/cakepie/chainData.js
+++ projects/cakepie/chainData.js
@@ -1,65 +1,16 @@
 async function getNativeToken(chainId) {
     const currentChainId = chainId ? chainId : getDefaultNetworkID();
-    if (isArbChain(currentChainId)) {
-      return "ETH";
-    } else if (isETHChain(currentChainId)) {
-      return "ETH";
-    } else if (isOptChain(currentChainId)) {
-      return "ETH";
-    } else if (isBSCChain(currentChainId)) {
-      return "BNB";
+    if (currentChainId == 42161) {
+        return "ETH";
+    } else if (currentChainId == 1) {
+        return "ETH";
+    } else if (currentChainId == 10) {
+        return "ETH";
+    } else if (currentChainId == 56) {
+        return "BNB";
     } else {
-      return "ETH";
+        return "ETH";
     }
-  };
-  
-  export const isArbChain = (chainId) => {
-    if (
-      chainId === networkIds.Arbitrum ||
-      chainId === networkIds.ArbitrumLOCAL ||
-      chainId === networkIds.ARB_GOERLI
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-  
-  export const isOptChain = (chainId) => {
-    console.log("running Opt Chain");
-    if (
-      chainId === networkIds.Optimism ||
-      chainId === networkIds.OptimismLOCAL
-      // chainId === networkIds.ARB_GOERLI
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-  
-  export const isETHChain = (chainId) => {
-    if (
-      chainId === networkIds.Ethereum ||
-      chainId === networkIds.EthereumLOCAL ||
-      chainId === networkIds.ETH_GOERLI
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-  
-  export const isBSCChain = (chainId) => {
-    if (
-      chainId === networkIds.BSCMain ||
-      chainId === networkIds.LOCAL ||
-      chainId === networkIds.BSCTestnet
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-  
+};
+
 module.exports = getNativeToken;
--- projects/cakepie/index.js
+++ projects/cakepie/index.js
@@ -1,19 +1,18 @@
-// const ADDRESSES = require('../helper/coreAssets.json')
-// const sdk = require('@defillama/sdk')
-// const { staking } = require('../helper/staking')
 const ERC20ABI = require("./abis/ERC20.json")
 const ethers = require('ethers')
+const sdk = require("@defillama/sdk");
 const pancakesdk = require("@pancakeswap/v3-sdk");
 const pancakev1Sdk = require("@pancakeswap/sdk");
-const fetchAllTokenPrice = require('./getTokenPrice.js');
+const getNativeToken = require('./chainData.js');
 const CakepieReaderAbi = require("./abis/CakepieReader.json");
 const config = require("./config")
 const _ = require("lodash")
 
 var WETHToken = "";
+var balances = {};
 
 async function getERC20TokenInfo(api, token) {
-  const tokenInfo = { "tokenAddress":"", "symbol": "", "decimals": 0 };
+  const tokenInfo = { "tokenAddress": "", "symbol": "", "decimals": 0 };
   if (token == "0x0000000000000000000000000000000000000000") return tokenInfo;
   tokenInfo.tokenAddress = token;
   if (token == "0x0000000000000000000000000000000000000001") {
@@ -26,20 +25,11 @@ async function getERC20TokenInfo(api, token) {
   return tokenInfo;
 }
 
-let AllTokenPricCache = null;
-const getAllTokenPrice = async () => {
-  if (AllTokenPricCache) {
-    return AllTokenPricCache;
-  }
-  AllTokenPricCache = fetchAllTokenPrice();
-  return AllTokenPricCache;
-};
-
 async function fetchTVLFromSubgraph(
+  api,
   pancakeStaking,
   pool
 ) {
-  const TokenPrice = await getAllTokenPrice();
   const response = await fetch(
     `https://api.thegraph.com/subgraphs/name/pancakeswap/masterchef-v3-bsc`,
     {
@@ -71,7 +61,6 @@ async function fetchTVLFromSubgraph(
     }
   );
   const result = await response.json();
-  let tvl = 0;
   if (result.data && result.data.userPositions) {
     const positionIDList = result.data.userPositions.map(item => {
       return item.id
@@ -120,44 +109,21 @@ async function fetchTVLFromSubgraph(
           pool: pool.v3PoolInfo.v3SDKPool,
           tickLower: Number(userPosition.tickLower.tickIdx),
           tickUpper: Number(userPosition.tickUpper.tickIdx),
-          liquidity: ethers.getBigInt(userPosition.liquidity),
+          liquidity: BigInt(userPosition.liquidity),
         });
-        // const token0 = getERC20TokenInfo(pool)
         const token0Amount = ethers.utils.parseUnits(
           pos.amount0.toFixed(),
           pool.v3PoolInfo.token0.decimals
         );
         const token1Amount = ethers.utils.parseUnits(
           pos.amount1.toFixed(),
-          pool.v3PoolInfo.token0.decimals
+          pool.v3PoolInfo.token1.decimals
         );
-        const token0Price =
-          TokenPrice[pool.v3PoolInfo.token0.symbol.toUpperCase()];
-        const token1Price =
-          TokenPrice[pool.v3PoolInfo.token1.symbol.toUpperCase()];
-        if (token0Price && token1Price) {
-          tvl =
-            tvl +
-            token0Price *
-            Number(
-              ethers.utils.formatUnits(
-                token0Amount,
-                pool.v3PoolInfo.token0.decimals
-              )
-            ) +
-            +token1Price *
-            Number(
-              ethers.utils.formatUnits(
-                token1Amount,
-                pool.v3PoolInfo.token1.decimals
-              )
-            );
-        }
+        sdk.util.sumSingleBalance(balances, pool.v3PoolInfo.token0.tokenAddress, token0Amount, api.chain);
+        sdk.util.sumSingleBalance(balances, pool.v3PoolInfo.token1.tokenAddress, token1Amount, api.chain);
       }
     }
   }
-  console.log(tvl)
-  return tvl;
 }
 
 async function getV3PoolInfo(
@@ -203,23 +169,24 @@ async function getV3PoolInfo(
     temptoken1.symbol,
     temptoken1.symbol
   );
+  v3PoolInfo.token0 = token0Token
   v3PoolInfo.token0.tokenAddress = token0Token.address;
-  v3PoolInfo.token0.symbol = token0Token.address;
-  
-  // : itemV3Pool.token0.isNative
-  //   ? getNativeToken(this.chainId)
-  //   : itemV3Pool.token0.symbol,
-  // decimals: itemV3Pool.token0.decimals.toNumber(),
-  // isNative: itemV3Pool.token0.isNative,
-
-  v3PoolInfo.token1 = token1Token
-
   if (v3PoolInfo.token0.tokenAddress == WETHToken) {
     v3PoolInfo.token0.isNative = true;
   }
+  v3PoolInfo.token0.symbol = v3PoolInfo.token0.isNative
+    ? await getNativeToken(api.chainId)
+    : v3PoolInfo.token0.symbol
+
+  v3PoolInfo.token1 = token1Token
+  v3PoolInfo.token1.tokenAddress = token1Token.address;
   if (v3PoolInfo.token1.tokenAddress == WETHToken) {
     v3PoolInfo.token1.isNative = true;
   }
+  v3PoolInfo.token1.symbol = v3PoolInfo.token1.isNative
+    ? await getNativeToken(api.chainId)
+    : v3PoolInfo.token1.symbol
+
   v3PoolInfo.pid = pid;
   var slot0 = {}
   var slot = await api.call({ abi: CakepieReaderAbi.slot0, target: V3poolInfo.poolAddress });
@@ -238,16 +205,16 @@ async function getV3PoolInfo(
   v3PoolInfo.farmCanBoost = await api.call({ abi: CakepieReaderAbi.whiteList, target: v3FARM_BOOSTER, params: pid });
   cakepiePool.v3PoolInfo = v3PoolInfo;
   cakepiePool.v3AccountInfo = await getV3AccountInfo(api, cakepiePool, PancakeStaking);
-  const v3Pool = new Pool(
+  const v3Pool = new pancakesdk.Pool(
     token0Token,
     token1Token,
-    v3PoolInfo.fee,
-    v3PoolInfo.slot0.sqrtPriceX96.toBigInt(),
-    v3PoolInfo.liquidity.toBigInt(),
+    parseInt(v3PoolInfo.fee, 10),
+    v3PoolInfo.slot0.sqrtPriceX96,
+    v3PoolInfo.liquidity,
     v3PoolInfo.slot0.tick,
     []
   );
-  cakepiePool.v3SDKPool = v3Pool;
+  cakepiePool.v3PoolInfo.v3SDKPool = v3Pool;
   return cakepiePool;
 }
 
@@ -280,7 +247,6 @@ async function getV3AccountInfo(
 async function getPoolInfo(api, PancakeStaking, masterChefV3, pancakeV3Helper, v3FARM_BOOSTER) {
   let poolsAdd = await api.fetchList({ lengthAbi: CakepieReaderAbi.poolLength, itemAbi: CakepieReaderAbi.poolList, target: PancakeStaking })
   let poolsInfo = await api.multiCall({ abi: CakepieReaderAbi.pools, calls: poolsAdd, target: PancakeStaking })
-  let pools = [];
   for (var i = 0; i < poolsAdd.length; i++) {
     let pool;
     if (poolsInfo[i].poolType == 1) {
@@ -288,10 +254,8 @@ async function getPoolInfo(api, PancakeStaking, masterChefV3, pancakeV3Helper, v
     } else if (poolsInfo[i].poolType == 2 || poolsInfo[i].poolType == 3) {
       pool = await getV2LikePoolInfo(poolsInfo[i], PancakeStaking);
     }
-    pools.push(pool);
-    await fetchTVLFromSubgraph(PancakeStaking, pool)
+    await fetchTVLFromSubgraph(api, PancakeStaking, pool)
   }
-  return pools
 }
 
 async function tvl(timestamp, block, chainBlocks, { api }) {
@@ -300,29 +264,11 @@ async function tvl(timestamp, block, chainBlocks, { api }) {
   const masterChefV3 = await api.call({ abi: CakepieReaderAbi.masterChefv3, target: CakepieReader })
   const pancakeV3Helper = await api.call({ abi: CakepieReaderAbi.pancakeV3Helper, target: CakepieReader })
   const v3FARM_BOOSTER = await api.call({ abi: CakepieReaderAbi.v3FARM_BOOSTER, target: CakepieReader })
-  const data = await getPoolInfo(api, PancakeStaking, masterChefV3, pancakeV3Helper, v3FARM_BOOSTER);
-  console.log("xxx", data);
-
-  //   const [poolTokens, depositTokens] = await getPoolList(api, MasterCakepieAddress, MCakeAddress, MCakeSVAddress, PancakeStaking);
-  //   console.log(depositTokens)
-  //   const decimals = await api.multiCall({ abi: 'erc20:decimals', calls: depositTokens })
-  const balances = {};
-  //   const cakeBal = await api.call({ abi: 'erc20:balanceOf', target: MCakeAddress, params: MasterCakepieAddress })
-  //   sdk.util.sumSingleBalance(balances, CakeAddress, cakeBal, api.chain)
-  //   if (MCakeSVAddress != ADDRESSES.null) {
-  //     const mCakeSVBal = await api.call({ abi: 'erc20:balanceOf', target: MCakeAddress, params: MCakeSVAddress })
-  //     sdk.util.sumSingleBalance(balances, CakeAddress, mCakeSVBal, api.chain)
-  //   }
-  //   const bals = await api.multiCall({ abi: 'erc20:balanceOf', calls: poolTokens.map(i => ({ target: i, params: MasterCakepieAddress })) })
-  //   bals.forEach((v, i) => {
-  //     v /= 10 ** (18 - decimals[i])
-  //     sdk.util.sumSingleBalance(balances, depositTokens[i], v, api.chain)
-  //   })
-  // return balances
+  await getPoolInfo(api, PancakeStaking, masterChefV3, pancakeV3Helper, v3FARM_BOOSTER);
+  return balances
 }
 
 Object.keys(config).forEach((chain) => {
-  const { CakepieReader } = config[chain];
   module.exports[chain] = {
     tvl: tvl,
     // staking: staking(CakepieReader)
